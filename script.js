fd7338e427bf66ab4e3c6f23411ad22affb57b8a
Samuel Ajala <samuelajala01@gmail.com>
2021-08-28 21:29:00 +0100

    edited the nav + made it responsive, position remaining

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
 function myalert(){
     alert("Site in progress");
 }
 
-setTimeout(myalert(), 2000);
-
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-
-toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active")
-})
