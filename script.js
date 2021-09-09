f7bd92ab2bfe9c4e53da73f6888987ec06709d43
Samuel Ajala <samuelajala01@gmail.com>
2021-09-09 04:04:07 +0100

    added favicon and changed IG link

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function myalert(){
-    alert("Adding finishing touches to site");
+function myalert() {
+  alert('Adding finishing touches to site');
 }
 myalert();
 
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+  navbarLinks.classList.toggle('active');
+});
 
 function cvClick() {
-    alert("You are now moving to another site");
+  alert('You are now moving to another site');
 }
