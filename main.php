cb018bd99b5f20c441e4962cb9c8c7a45345850b
Samuel <samuelajala01@gmail.com>
2021-10-11 01:33:31 +0100

    added mail php for form

--- main.php
+++ main.php
@@ -1,11 +1,17 @@
-<?php $name = $_POST['name'];
-$number = $_POST['number'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent = "From: $name\n Number: $number \n Message: $message";
-$recipient = "samuelajala01@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+
+if(isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $number = $_POST['number'];
+  $maiFrom = $_POST['email'];
+  $message = $_POST['message'];
+  
+  $mailTo = "";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an email from ".$name.".\n\n".$message;
+
+  mail($mailTo, $number, $txt, $headers);
+  headers("Location: mail.php?mailsend");
+}
+
 ?>
