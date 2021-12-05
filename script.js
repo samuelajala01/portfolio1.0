ed04479d1bec5b88512164e8caab5469458c3cb3
Samuel Ajala <samuelajala01@gmail.com>
2021-12-05 14:57:45 +0100

    added first few animations

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-setTimeout(function () {
-  alert('working on the form submission and social links');
-}, 5000);
+//setTimeout(function () {
+  //alert('working on the form submission and social links');
+//}, 5000);
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
